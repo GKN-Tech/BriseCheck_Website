8d07a10e07707527e5bfcdd6202caa05b746b431
Gurkirat Kaur <63808362+GKN-Tech@users.noreply.github.com>
2024-02-24 17:31:51 -0800

    use environment variable PORT value

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from website import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    # environ to make it work for dynamically assigned ports on hosting platform
+    app.run(debug=True, host='0.0.0.0', port=port)
     
