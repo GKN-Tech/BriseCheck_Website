361eb315e4a604add93201cc4659a16d6507fab1
Gurkirat Kaur <63808362+GKN-Tech@users.noreply.github.com>
2024-02-24 17:27:54 -0800

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'brisecheck@ismyschoolproject'
+    app.config['SECRET_KEY'] = 'brisecheck@ismyfunproject'
 
     from .views import views
     from .pages import pages
